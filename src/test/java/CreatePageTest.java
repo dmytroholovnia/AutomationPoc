8614aa03d8ee82b636a84e4d35c66376709ac5d3
dmytro.holovnia <dmytro.holovnia@gmail.com>
2024-09-27 10:13:31 +0300

    added payment data setup, remover ordered test dependency

--- src/test/java/CreatePageTest.java
+++ src/test/java/CreatePageTest.java
@@ -2,55 +2,44 @@ import datagenerator.PageDataGenerator;
 import dto.createpage.CreatePageRequestDto;
 import dto.status.StatusRequestDto;
 import enums.Status;
-import org.junit.jupiter.api.*;
-import ui.PaymentPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import ui.PaymentSuccessPage;
 import utils.CurrencyFormatter;
 
-import static datagenerator.CardDataGenerator.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CreatePageTest extends BaseTest {
 
-    private static CreatePageRequestDto createPageRequestDto;
+    private CreatePageRequestDto createPageRequestDto;
+    private PaymentSuccessPage paymentSuccessPage;
 
-    @BeforeAll
-    public static void setupData() {
+    @BeforeEach
+    public void setupOrder() {
         createPageRequestDto = PageDataGenerator.generateDto();
+        var responseDto = apiService.createPage(createPageRequestDto);
+        String orderUrl = responseDto.getUrl();
+        paymentSuccessPage = new PaymentDataSetup(coreDriver).applyPayment(orderUrl);
     }
 
     @Test
-    @Order(1)
     @DisplayName("Create payment page test")
     public void createPageTest() {
-        var responseDto = apiService.createPage(createPageRequestDto);
-        assertThat(responseDto.getUrl()).isNotEmpty();
-        String orderUrl = responseDto.getUrl();
-
-        PaymentSuccessPage paymentSuccessPage = new PaymentPage(coreDriver.getDriver())
-                .open(orderUrl)
-                .enterCardNumber(getTestCardNumber())
-                .enterExpiryDate(generateExpiryDate())
-                .enterCvv(generateCvv())
-                .enterEmail(generateEmail())
-                .submit();
         assertThat(paymentSuccessPage.getStatusTitle().isDisplayed()).isTrue();
-
         var expectedPrice = CurrencyFormatter.formatCurrencyToUi(createPageRequestDto);
         assertThat(paymentSuccessPage.getOrderPrice().getText())
                 .isEqualTo(expectedPrice);
     }
 
     @Test
-    @Order(2)
     @DisplayName("Get order status test")
     public void getStatusTest() {
         StatusRequestDto statusRequestDto = new StatusRequestDto(createPageRequestDto.getOrder().getOrderId());
-        var responseDto = apiService.getStatus(statusRequestDto);
-        assertThat(responseDto.getOrder().getAmount()).isEqualTo(createPageRequestDto.getOrder().getAmount());
-        assertThat(responseDto.getOrder().getCurrency()).isEqualTo(createPageRequestDto.getOrder().getCurrency());
-        assertThat(responseDto.getOrder().getStatus()).isEqualTo(Status.APPROVED.getValue());
+        var orderDto = apiService.getStatus(statusRequestDto).getOrder();
+        assertThat(orderDto.getAmount()).isEqualTo(createPageRequestDto.getOrder().getAmount());
+        assertThat(orderDto.getCurrency()).isEqualTo(createPageRequestDto.getOrder().getCurrency());
+        assertThat(orderDto.getStatus()).isEqualTo(Status.APPROVED.getValue());
     }
 
 }
